513c8900f1544417fbbd7caae2a1b242f816c53b
LunaRossie <>
2022-07-27 10:21:25 -0500

    add to file

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 // Import our modular routers for /tips and /feedback
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
+const deleteRouter = require ('./delete');
 
 const app = express();
 
 app.use('/tips', tipsRouter);
 app.use('/feedback', feedbackRouter);
+app.use('/delete', deleteRouter);
 
 module.exports = app;
